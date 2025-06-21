4fda8268fa7c408fd21158bfdacbed3a98c5a926
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-06-22 03:48:00 +0700

    Create creator-rewards.js

---
+++ api/creator-rewards.js
@@ -0,0 +1,20 @@
+// api/creator-rewards.js
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  try {
+    const periodsAgo = req.query.periodsAgo || "0";
+    const resp = await fetch(
+      `https://api.farcaster.xyz/v1/creator-rewards-winner-history?periodsAgo=${periodsAgo}`
+    );
+
+    if (!resp.ok) {
+      return res.status(resp.status).send(`Error ${resp.statusText}`);
+    }
+
+    const data = await resp.json();
+    res.status(200).json(data.result.winners || []);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
