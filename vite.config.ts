db42399c679c752168fb864f2cfc480c17e82bc3
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-06-22 05:07:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: true,
+  },
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
   },
 });
